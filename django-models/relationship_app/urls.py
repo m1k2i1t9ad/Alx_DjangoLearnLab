d8554b8b8d20dc08216cbb86f36d40d385adeeb7
m1k2i1t9ad <michaelt5593@gmail.com>
2024-11-09 19:42:20 +0300

    Implement role-based access control: add UserProfile model, roles, and role-specific views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 from .views import register
 from .views import list_books, LibraryDetailView, user_login,user_logout
 from django.contrib.auth.views import LoginView, LogoutView  # Import class-based views for login/logout
+from .views import admin_view, librarian_view, member_view
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # URL for login
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # URL for logout
-    path('register/',views.register, name='register'),  # URL for registration
+    path('register/',register, name='register'),  # URL for registration
+    
+    path('admin/', admin_view, name='admin_view'),  # URL for Admin view
+    path('librarian/', librarian_view, name='librarian_view'),  # URL for Librarian view
+    path('member/', member_view, name='member_view'),  # URL for Member view
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,6 +8,8 @@ from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 from django.http import HttpResponse
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
 
 # Function-based view to list all books
 def list_books(request):
@@ -47,3 +49,28 @@ def user_login(request):
 def user_logout(request):
     logout(request)  # Log the user out
     return render(request, 'relationship_app/logout.html')  # Render the logout confirmation template
+
+
+# Function to check if user is an Admin
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+# Function to check if user is a Librarian
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+# Function to check if user is a Member
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')  # Render the admin view template
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')  # Render the librarian view template
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')  # Render the member view template
