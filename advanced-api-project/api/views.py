1d9deae61236df45093439e4607c14b7d4959430
m1k2i1t9ad <michaelt5593@gmail.com>
2024-12-02 14:19:43 +0300

    Implement generic views for Book model and set up CRUD operations

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 class ListView(generics.ListCreateAPIView):
     """View to list all books and create a new book."""
