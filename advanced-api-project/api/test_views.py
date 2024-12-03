f2bd2e46ea2250d462d63151454738f924279f29
m1k2i1t9ad <michaelt5593@gmail.com>
2024-12-03 09:01:24 +0300

    Added unit test for creating a book

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,18 +1,24 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
 from .models import Book
-from .serializers import BookSerializer
 
 class BookTest(APITestCase):
-  def test_create_book(self):
-    data = {'title': 'Test Book', 'author': 'John Doe', 'publication_year': 2023}
-    url = reverse('book-list')
-    response = self.client.post(url, data, format='json')
-    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    self.assertEqual(Book.objects.count(), 1)
-    book = Book.objects.get(pk=response.data['id'])
-    self.assertEqual(book.title, data['title'])
-    self.assertEqual(book.author, data['author'])
-    self.assertEqual(book.publication_year, data['publication_year'])
+    def setUp(self):
+        # Create a user for authentication
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_create_book(self):
+        data = {'title': 'Test Book', 'author': 'John Doe', 'publication_year': 2023}
+        url = reverse('book-list')
+        response = self.client.post(url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 1)
+        book = Book.objects.get(pk=response.data['id'])
+        self.assertEqual(book.title, data['title'])
+        self.assertEqual(book.author, data['author'])
+        self.assertEqual(book.publication_year, data['publication_year'])
