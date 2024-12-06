76075eaaa970ed5d29df044bb2a793802865389d
m1k2i1t9ad <michaelt5593@gmail.com>
2024-12-06 20:15:26 +0300

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path  # Import path function for URL patterns
 from . import views  # Import views from the current app
 from django.contrib.auth import views as auth_views  # Import built-in auth views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,CommentCreateView,CommentUpdateView,CommentDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,CommentCreateView,CommentUpdateView,CommentDeleteView,SearchView,PostsByTagView
 
 app_name = 'blog'
 urlpatterns = [
@@ -19,5 +19,5 @@ urlpatterns = [
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_edit'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
     path('search/', SearchView.as_view(), name='search_posts'),  # Search URL
-    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts_by_tag'),  # URL for posts by tag
+    path('tags/<str:tag_name>/', views.PostsByTagView.as_view(), name='posts_by_tag'),  # URL for posts by tag
 ]
