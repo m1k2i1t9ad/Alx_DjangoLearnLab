269c32befe3f78ebc1989f414efb48809181c2c0
m1k2i1t9ad <michaelt5593@gmail.com>
2024-12-12 21:30:32 +0300

    Implementing User Follows and Feed Functionali

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -24,7 +24,7 @@ class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_follow = get_object_or_404(User, id=user_id)
+        user_to_follow = get_object_or_404(User.objects.all(), id=user_id)
         request.user.following.add(user_to_follow)
         return Response({"message": f"You are now following {user_to_follow.username}."})
 
@@ -32,6 +32,6 @@ class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_unfollow = get_object_or_404(User, id=user_id)
+        user_to_unfollow = get_object_or_404(User.objects.all(), id=user_id)
         request.user.following.remove(user_to_unfollow)
         return Response({"message": f"You have unfollowed {user_to_unfollow.username}."})
