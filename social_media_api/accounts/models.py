d92a96ef58b17710063562c8fdb1bc6854d11b2a
m1k2i1t9ad <michaelt5593@gmail.com>
2024-12-12 20:45:03 +0300

    Update User model to use ImageField for profile_picture

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     bio = models.TextField(blank=True, null=True)
-    profile_picture = models.URLField(blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='followed_by', blank=True)
 
     def __str__(self):
         return self.username
