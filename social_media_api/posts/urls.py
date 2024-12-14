cc5e94afff54417c37844fd556679b69376c9f5e
m1k2i1t9ad <michaelt5593@gmail.com>
2024-12-14 17:49:14 +0300

    Implementing User Follows and Feed Functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -5,8 +5,9 @@ from .views import PostViewSet, CommentViewSet,FeedViewSet
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
-router.register(r'feed', FeedViewSet,basename='feed')
+# router.register(r'feed', FeedViewSet,basename='feed')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('feed/', FeedViewSet.as_view({'get': 'list'}), name='feed'),  # Explicitly add the feed endpoint
 ]
